a073e73901acb4d1f2e3d5071af63d65c4553e65
kulloveth <kulloveth@gmail.com>
2021-11-19 09:12:40 +0100

    login ui

--- app/src/main/java/eu/tutorials/instagramclone/MainActivity.kt
+++ app/src/main/java/eu/tutorials/instagramclone/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import eu.tutorials.instagramclone.ui.screen.Login
 import eu.tutorials.instagramclone.ui.theme.IntagramCloneTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +18,7 @@ class MainActivity : ComponentActivity() {
             IntagramCloneTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    InstagramApp()
                 }
             }
         }
@@ -25,14 +26,13 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun InstagramApp() {
+    Login()
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     IntagramCloneTheme {
-        Greeting("Android")
     }
 }
