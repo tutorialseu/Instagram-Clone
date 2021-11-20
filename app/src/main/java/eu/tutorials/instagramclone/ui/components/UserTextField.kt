40fe0e4103d84bad149ededa256fe8d1979a448c
kulloveth <kulloveth@gmail.com>
2021-11-20 12:26:53 +0100

    create instagram login ui

--- app/src/main/java/eu/tutorials/instagramclone/ui/components/UserTextField.kt
+++ app/src/main/java/eu/tutorials/instagramclone/ui/components/UserTextField.kt
@@ -9,10 +9,22 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 
+/**Todo 8: create a reusable composable for the username and password field
+ * @param [fieldState] holds the entered value
+ * @param onFieldChange keeps track of when the entered value changes
+ * @param placeholder holds the fields hint word
+ * @param modifier  sets the properties of both element
+ * @param passwordToggle changes  the trailing icon
+ * @param passwordTransformation sets the password value visibility
+ */
 @Composable
 fun UserTextField(fieldState:String, onFieldChange:(String)->Unit, placeholder:String,
                   modifier: Modifier = Modifier,
                   passwordToggle: @Composable (() -> Unit)? = null,passwordTransformation:VisualTransformation = VisualTransformation.None) {
+    /*Todo 9: We add an OutlineTextField with place holder,trailing icon which is st to null by default
+    since we only need it for password field, visual transformation which is set
+    to None by default so we can ignore it for the user name field
+     */
     OutlinedTextField(value = fieldState, onValueChange = {
        onFieldChange(it)
     },placeholder = { Text(text =placeholder) },
--- app/src/main/java/eu/tutorials/instagramclone/ui/screen/Login.kt
+++ app/src/main/java/eu/tutorials/instagramclone/ui/screen/Login.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Facebook
@@ -25,8 +26,13 @@ import androidx.compose.ui.unit.dp
 import eu.tutorials.instagramclone.R
 import eu.tutorials.instagramclone.ui.components.UserTextField
 
+/**Todo 1: create a screen package and create a login file in it
+ * create a Login composable with a modifier parameter that we will use for every other element
+ */
 @Composable
 fun Login(modifier: Modifier = Modifier) {
+    //Todo 10: we create a state variable for the username, password and visibility icon toggle
+    //start
     val usernameState = remember {
         mutableStateOf("")
     }
@@ -36,38 +42,76 @@ fun Login(modifier: Modifier = Modifier) {
     val toggleState = remember {
         mutableStateOf(false)
     }
+    //end
+    //Todo 20: create a scrollable state
     val scrollState = rememberScrollState()
+
+    /*Todo 2: add a Column as the parent layout with a modifier to fillMaxSize ie height and width
+      Then a vertical padding of 16dp ie both top and bottom space
+     */
     Column(
         modifier = modifier
             .fillMaxSize()
             .padding(vertical = 16.dp)
     ) {
+        /*Todo 4: Add a Row for language selection and set to fillMaxWidth
+         with a horizontalArrangement to Center
+         Add a Text with text set to English
+         An Icon set to KeyboardArrowDown
+         */
         Row(
             modifier = modifier
                 .fillMaxWidth()
-                .weight(1.0f), horizontalArrangement = Arrangement.Center
+                    //Todo 12: Add weight for the language drop down
+                    //start
+                .weight(1.0f),
+                   //end
+            horizontalArrangement = Arrangement.Center
         ) {
             Text(text = "English (United States)")
             Icon(imageVector = Icons.Outlined.KeyboardArrowDown, contentDescription = "")
         }
 
+        /*Todo 5:Add a column for the next section of the UI
+        so we can group them together and set appropriate space between
+        the Instagram image and the language drop down
+        We set the width to fillMaxWidth, a horizontal padding of 16.dp
+        and weight of 5.0f
+        */
         Column(
             modifier = modifier
                 .fillMaxWidth()
                 .padding(horizontal = 16.dp)
-                .scrollable(scrollState,Orientation.Vertical)
+                    //Todo 21:add vertical scrolling
+                    //start
+                .verticalScroll(scrollState)
+                    //end
                 .weight(5.0f)
         ) {
+            //Todo 6: add an Image and set the instagram drawable to fillMaxWidth
             Image(
                 painter = painterResource(id = R.drawable.instagram), contentDescription = "",
                 modifier = modifier
                     .fillMaxWidth()
             )
+            /*Todo 11: We add the first reusable element we created for the username,set fieldState
+               to usernameState value, in onfieldChange we get the string and
+               set the usernameState and then set a placeholder
+             */
             UserTextField(
                 fieldState = usernameState.value,
                 onFieldChange = { usernameState.value = it },
                 placeholder = "Phone number, email or username"
             )
+
+            /*Todo 12: add the reusable element again this time for the password
+              set the fieldState to passwordState,in onfieldChange we the value from the field
+              and set to passwordState, for passwordToggle we set an IconButton
+              and in the onCLick turn toggle state to true or false, now for the Icon
+              we check if the togglestate is true we turn on visibility else we turn off visibility
+              And then for password transformation we set it none when togglestate is true
+              and to password transformation when its false
+              */
             UserTextField(
                 fieldState = passwordState.value,
                 onFieldChange = { passwordState.value = it },
@@ -83,16 +127,28 @@ fun Login(modifier: Modifier = Modifier) {
                 passwordTransformation = if (toggleState.value) VisualTransformation.None else PasswordVisualTransformation()
             )
 
-            OutlinedButton(
+            /*
+              Todo 13: we add a Button for Login, the onclick button is not implementedd yet
+              we se modifier to fillMaxWidth with a top padding of 16dp.
+              Now we only want the button to be enabled when the fields are not empty just like on instagram
+              we get the state for each field created above and check if both is not empty then enable the button
+             Then we set the text to Login and add a vertical padding
+             */
+            Button(
                 onClick = { },
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(top = 16.dp),
-                enabled = usernameState.value.isNotEmpty() && passwordState.value.isNotEmpty(),
+                enabled = usernameState.value.isNotEmpty() && passwordState.value.isNotEmpty()
             ) {
                 Text(text = "Login", modifier = modifier.padding(vertical = 8.dp))
             }
 
+            /*
+            Todo 14 We add a row for forgot details,set modifier to fillMaxWidth and padding top to
+            16dp then add horizontalArrangement to Center
+            In the row we add two Text element that is required
+             */
             Row(
                 modifier = modifier
                     .fillMaxWidth()
@@ -101,6 +157,11 @@ fun Login(modifier: Modifier = Modifier) {
                 Text(text = "forgot your login details? ")
                 Text(text = "Get help Signing in.", fontWeight = FontWeight.Bold)
             }
+            /*
+            Todo 15 we use a Row to set two dividers at the start and end of the OR Text, using the
+            weight modifier we git the dividers 5.0 weight each and then the Text stays at the middle.
+            We also add a top padding of 12dp to keep them at the center of the Text
+             */
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -118,6 +179,11 @@ fun Login(modifier: Modifier = Modifier) {
                         .padding(top = 12.dp)
                 )
             }
+            /*
+            todo 16: next is the facebook login option which we add a row and set the icon and text
+             side by side. Then the Row gets a fillMaxWidth and padding top of 16dp
+             */
+
             Row(
                 modifier = modifier
                     .fillMaxWidth()
@@ -129,13 +195,17 @@ fun Login(modifier: Modifier = Modifier) {
                     contentDescription = "",
                     tint = Color.Blue
                 )
-                Text(text = " Login With Facebook ")
+                Text(text = " Login With Facebook ",color = Color.Blue)
             }
 
         }
 
+        //Todo 17: We set a line using  a Divider and add a top padding of 48dp
         Divider(modifier = modifier.padding(top = 48.dp))
 
+        /* Todo 18: We add a Row with two Text for new account SignUp
+        add a modifier with fillMaxWidth and top padding of 16.dp with horizontalArrangement of Center
+         */
         Row(
             modifier = modifier
                 .fillMaxWidth()
@@ -148,7 +218,7 @@ fun Login(modifier: Modifier = Modifier) {
     }
 }
 
-
+//Todo 7 we add a preview for the Login
 @Preview(showBackground = true)
 @Composable
 fun LoginPreview() {
