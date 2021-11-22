dec9cc69bf84759e7b6000c66b785ba7b1c812c2
kulloveth <kulloveth@gmail.com>
2021-11-22 09:02:21 +0100

    add bottom bar

---
+++ app/src/main/java/eu/tutorials/instagramclone/model/Bottommenu.kt
@@ -0,0 +1,15 @@
+package eu.tutorials.instagramclone.model
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.outlined.*
+import androidx.compose.ui.graphics.vector.ImageVector
+
+//Todo 3: create a class with fields for each menu item
+sealed class BottomMenu(val route:String, val label:String, val icon: ImageVector, val selectedIcon: ImageVector){
+    object Home:BottomMenu("home","Home", Icons.Outlined.Home, Icons.Filled.Home)
+    object Search:BottomMenu("search","Search",Icons.Outlined.Search,Icons.Filled.Search)
+    object Reels:BottomMenu("reels","Reels",  Icons.Outlined.OndemandVideo,Icons.Filled.OndemandVideo)
+    object Activity:BottomMenu("activity","Activity", Icons.Outlined.FavoriteBorder,Icons.Filled.Favorite)
+    object Account:BottomMenu("account","Account",Icons.Outlined.AccountCircle,Icons.Filled.AccountCircle)
+}
